77c56caff8c60b4936dcf4dca3192eaff0cc33c4
manortbla <manortbla@alum.us.es>
2024-02-22 11:22:37 +0100

    Co-authored-by: pedlopruz <pedlopruz@users.noreply.github.com> Co-authored-by: JuanluRM <JuanluRM@users.noreply.github.com> feat: Create models of user, family, partner and volunteer

--- nexong/models.py
+++ nexong/models.py
@@ -1,35 +1,126 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone 
+
 # Create your models here.
 
 ADMIN = 'ADMIN'
-VOLUNTEER = 'VOLUNTEER'
 EDUCATOR = 'EDUCATOR'
+VOLUNTEER = 'VOLUNTEER'
 FAMILY = 'FAMILY'
 PARTNER = 'PARTNER'
+VOLUNTEER_PARTNER= 'VOLUNTEER_PARTNER'
 
-PLAN_CHOICES = [
-    (ADMIN, 'Admin'),
+ROLE = [
+    (ADMIN, 'Administrador'),
     (VOLUNTEER, 'Voluntario'),
     (EDUCATOR, 'Educador'),
     (FAMILY, 'Familia'),
     (PARTNER, 'Socio'),
+    (VOLUNTEER_PARTNER, 'Voluntario y socio'),
+]
+PENDING = 'PENDING'
+ACCEPTED = 'ACCEPTED'
+REJECTED = 'REJECTED'
+
+STATUS = [
+    (PENDING, 'Pendiente'),
+    (ACCEPTED, 'Aceptado'),
+    (REJECTED, 'Rechazado'),
+]
+ANNUAL = 'ANNUAL'
+MENSUAL = 'MENSUAL'
+QUARTERLY = 'QUARTERLY'
+SIXMONTHLY = 'SIX-MONTHLY'
+FRECUENCY = [
+    (ANNUAL, 'Anual'),
+    (MENSUAL, 'Mensual'),
+    (QUARTERLY, 'Trimestral'),
+    (SIXMONTHLY, ' Seis Meses'),
+]
+THREE_YEARS = 'THREE_YEARS'
+FOUR_YEARS = 'FOUR_YEARS'
+FIVE_YEARS = 'FIVE_YEARS'
+FIRST_PRIMARY = 'FIRST_PRIMARY'
+SECOND_PRIMARY = 'SECOND_PRIMARY'
+THIRD_PRIMARY = 'THIRD_PRIMARY'
+FOURTH_PRIMARY = 'FOURTH_PRIMARY'
+FIFTH_PRIMARY = 'FIFTH_PRIMARY'
+SIXTH_PRIMARY = 'SIXTH_PRIMARY'
+FIRST_SECONDARY = 'FIRST_SECONDARY'
+SECOND_SECONDARY = 'SECOND_SECONDARY'
+THIRD_SECONDARY= 'THIRD_SECONDARY'
+FOURTH_SECONDARY= 'FOURTH_SECONDARY'
+
+CURRENTEDUCATIONYEAR=[
+    (THREE_YEARS, 'Tres años'),
+    (FOUR_YEARS, 'Cuatro años'),
+    (FIVE_YEARS, 'Cinco años'),
+    (FIRST_PRIMARY, 'Primero de primaria'),
+    (SECOND_PRIMARY, 'Segundo de primaria'),
+    (THIRD_PRIMARY, 'Tercero de primaria'),
+    (FOURTH_PRIMARY, 'Cuarto de primaria'),
+    (FIFTH_PRIMARY, 'Quinto de primaria'),
+    (SIXTH_PRIMARY, 'Sexto de primaria'),
+    (FIRST_SECONDARY, 'Primero de secundaria'),
+    (SECOND_SECONDARY, 'Segundo de secundaria'),
+    (THIRD_SECONDARY, 'Tercero de secundaria'),
+    (FOURTH_SECONDARY,'Cuarto de secundaria'), 
 ]
 
-class User(AbstractBaseUser):
+class Family(models.Model):
+    name = models.CharField(max_length=255)
+    documents = models.CharField(max_length=250)
 
-    username = models.CharField(max_length=400, unique=True)
+class Partner(models.Model):
+    holder = models.CharField(max_length=50, unique=True)
+    iban = models.CharField(max_length=34, unique=True)
+    frecuency = models.CharField(max_length=11,choices=FRECUENCY, default=MENSUAL)
+    total = models.FloatField()
+    documents = models.CharField(max_length=250)
+
+class Volunteer(models.Model):
+    academic_formation=models.CharField(max_length=1000)
+    motivation=models.CharField(max_length=1000)
+    status=models.CharField(
+        max_length=10, 
+        choices=STATUS,
+        default=PENDING)
+    verify=models.BooleanField(default=False)
+    documents = models.CharField(max_length=100)
+
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=50, blank = True)
+    surname = models.CharField(max_length=100, blank = True)
+    nationalID = models.CharField(max_length=9, unique=True,blank = True)
+    nationality = models.CharField(max_length=50,blank = True)
+    educativeCentre = models.CharField(max_length=100, blank = True)
+    educativeCentreTutor = models.CharField(max_length=50, blank = True)
+    currentEducationYear = models.CharField(
+        max_length=25,
+        choices=CURRENTEDUCATIONYEAR,
+        default=THREE_YEARS,
+    )
+    birthdate = models.DateField(null=True)
+    phone = models.IntegerField(validators=[MinValueValidator(9),MaxValueValidator(9)], default = '000000000')
     email = models.EmailField(unique=True)
-    avatar = models.URLField(max_length=255)
-    password = models.CharField(max_length=500)
-    address = models.CharField(max_length=255)
-    telephone = models.CharField(max_length=20)
-    rol = models.CharField(
-        max_length=10,
-        choices=PLAN_CHOICES,
+    role = models.CharField(
+        max_length=25,
+        choices=ROLE,
         default=FAMILY,
     )
+    password = models.CharField(max_length=500, null = False)
+    avatar=models.ImageField()
+    address = models.CharField(max_length=255, blank=True)
+    postal_code= models.IntegerField(validators=[
+        MinValueValidator(5),
+        MaxValueValidator(5)], default = '00000')
+    family = models.ForeignKey(Family, on_delete=models.CASCADE, null=True)
+    partner = models.ForeignKey(Partner, on_delete=models.CASCADE, null=True)
+    volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE, null=True)
     last_login = None
     is_active = models.BooleanField(default=False)
 
