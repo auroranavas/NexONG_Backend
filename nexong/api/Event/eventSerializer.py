aaf91b56f985767d39600e81cf3574decf30ee87
Ahydul <ahydul1@gmail.com>
2024-02-24 13:59:05 +0100

    fix: Event Crud now works properly

--- nexong/api/Event/eventSerializer.py
+++ nexong/api/Event/eventSerializer.py
@@ -4,8 +4,18 @@ from rest_framework.serializers import ModelSerializer
 
 
 class EventSerializer(ModelSerializer):
+    educators = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.filter(educator__isnull=False))
+    
     class Meta:
         model = Event
         fields = ['name','description','place','capacity','max_volunteers',
         'start_date','end_date','lesson', 'educators']
 
+    def get_educators(self, obj):
+        educators = User.objects.filter(educator__isnull=False)
+        return educators
+
+    def validate_educators(self, value):
+        if not value:
+            raise serializers.ValidationError("Must provide at least 1 educator.")
+        return value
