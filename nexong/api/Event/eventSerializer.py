7dec26124a15f81a753d3fa4a6472ee08fdd6195
Ahydul <ahydul1@gmail.com>
2024-02-24 22:57:50 +0100

    fix: change wrong propperty name

--- nexong/api/Event/eventSerializer.py
+++ nexong/api/Event/eventSerializer.py
@@ -6,6 +6,14 @@ from rest_framework.serializers import ModelSerializer
 class EventSerializer(ModelSerializer):
     class Meta:
         model = Event
-        fields = ['name','description','place','capacity','max_volunteers',
-        'start_date','end_date','lesson', 'educators']
-
+        fields = [
+            "name",
+            "description",
+            "place",
+            "capacity",
+            "max_volunteers",
+            "start_date",
+            "end_date",
+            "lesson",
+            "attendees",
+        ]
