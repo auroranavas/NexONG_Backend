bee60610427f092baea6130d6d2368b96091346f
Ahydul <ahydul1@gmail.com>
2024-02-25 13:15:05 +0100

    fix: Remove unused

--- nexong/api/routers.py
+++ nexong/api/routers.py
@@ -1,9 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from .Authentication.views import *
 from .Meeting.views import *
-from .Event.views import EventApiViewSet
 
 router_api = DefaultRouter()
-router_api.register(prefix='user', viewset=UserApiViewSet, basename='user')
-router_api.register(prefix='event', viewset=EventApiViewSet, basename='event')
+router_api.register(prefix="user", viewset=UserApiViewSet, basename="user")
 router_api.register(prefix="meeting", viewset=MeetingApiViewSet, basename="meeting")
