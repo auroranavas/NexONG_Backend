a797130d5a3c92c99d57475a195e3fd0771a5c2c
Ahydul <ahydul1@gmail.com>
2024-02-25 13:15:24 +0100

    feat: Add id and url to Meeting serializer

--- nexong/api/Meeting/meetingSerializer.py
+++ nexong/api/Meeting/meetingSerializer.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
-from nexong.models import User
+from nexong.models import Partner
 from rest_framework.serializers import ModelSerializer
 from nexong.models import Meeting
 
 
 class MeetingSerializer(ModelSerializer):
     attendees = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=User.objects.filter(student__isnull=False)
+        many=True, required=False, queryset=Partner.objects.all()
     )
+    url = serializers.HyperlinkedIdentityField(view_name="meeting-detail")
 
     class Meta:
         model = Meeting
-        fields = ["name", "description", "date", "time", "attendees"]
+        fields = ["id", "name", "description", "date", "time", "attendees", "url"]
