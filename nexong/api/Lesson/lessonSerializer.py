9ec8144da651d744f9ea342bfc201659ddd41922
pedlopruz <plopezr2011@gmail.com>
2024-02-28 13:49:50 +0100

    feat: Lesson Crud Added

---
+++ nexong/api/Lesson/lessonSerializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from nexong.models import Lesson, Student
+from rest_framework.serializers import ModelSerializer
+
+
+class LessonSerializer(ModelSerializer):
+    students = serializers.PrimaryKeyRelatedField(
+        many=True, required=False, queryset=Student.objects.all()
+    )
+    url = serializers.HyperlinkedIdentityField(view_name="meeting-detail")
+
+    class Meta:
+        model = Lesson
+        fields = ["id", "name", "description", "capacity", "is_morning_lesson",  "educator","students","url"]
--- nexong/api/routers.py
+++ nexong/api/routers.py
@@ -1,8 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from .Authentication.views import *
 from .Meeting.views import *
+from .Event.views import *
+from .Lesson.views import *
 
 router_api = DefaultRouter()
 router_api.register(prefix="user", viewset=UserApiViewSet, basename="user")
 router_api.register(prefix="meeting", viewset=MeetingApiViewSet, basename="meeting")
 router_api.register(prefix="event", viewset=EventApiViewSet, basename="event")
+router_api.register(prefix="lesson", viewset=LessonApiViewSet, basename="lesson")
