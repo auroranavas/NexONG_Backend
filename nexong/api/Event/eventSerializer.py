1c09a2614aa6fe288aca0b59be66ea6153e6a351
Ahydul <ahydul1@gmail.com>
2024-03-02 13:12:58 +0100

    fix: Update event serializer to the changes to db

--- nexong/api/Event/eventSerializer.py
+++ nexong/api/Event/eventSerializer.py
@@ -1,7 +1,66 @@
 from rest_framework import serializers
-from nexong.models import Event, User
+from nexong.models import Event, User, LessonEvent
 from rest_framework.serializers import ModelSerializer
 from datetime import datetime, timezone
+
+
+class LessonEventSerializer(ModelSerializer):
+    # attendees is optional at creation
+    attendees = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), many=True, required=False
+    )
+    url = serializers.HyperlinkedIdentityField(view_name="lessonevent-detail")
+
+    class Meta:
+        model = LessonEvent
+        fields = [
+            "id",
+            "name",
+            "description",
+            "place",
+            "max_volunteers",
+            "start_date",
+            "end_date",
+            "lesson",
+            "price",
+            "educators",
+            "attendees",
+            "volunteers",
+            "url",
+        ]
+
+    def validate_educators(self, value):
+        if not value:
+            raise serializers.ValidationError("Must provide at least 1 educator.")
+        return value
+
+    # Validations that depends on more than one parameter
+    def validate(self, attrs):
+        validation_error = {}
+
+        volunteers_emails = attrs.get("volunteers")
+        num_volunteers = len(volunteers_emails)
+        max_volunteers = attrs.get("max_volunteers")
+
+        if max_volunteers < num_volunteers:
+            validation_error["max_volunteers"] = (
+                "max_volunteers must be higher or equal to the number of volunteers selected."
+            )
+
+        start_date = attrs.get("start_date")
+        if start_date <= datetime.now(timezone.utc):
+            validation_error["start_date"] = "The start date must be in the future."
+
+        end_date = attrs.get("end_date")
+        if end_date <= datetime.now(timezone.utc):
+            validation_error["end_date"] = "The end date must be in the future."
+        if end_date <= start_date:
+            validation_error["end_date"] = "The end date must be after the start date."
+
+        if validation_error:
+            raise serializers.ValidationError(validation_error)
+
+        return attrs
 
 
 class EventSerializer(ModelSerializer):
@@ -20,31 +79,47 @@ class EventSerializer(ModelSerializer):
             "place",
             "capacity",
             "max_volunteers",
+            "max_attendees",
             "start_date",
             "end_date",
-            "lesson",
+            "price",
             "attendees",
-            "educators",
+            "volunteers",
             "url",
         ]
 
-    def validate_educators(self, value):
-        if not value:
-            raise serializers.ValidationError("Must provide at least 1 educator.")
-        return value
-
+    # Validations that depends on more than one parameter
     def validate(self, attrs):
         validation_error = {}
 
         attendees_emails = attrs.get("attendees")
-        attendees_qs = User.objects.filter(email__in=attendees_emails)
+        num_attendees = len(attendees_emails)
+        max_attendees = attrs.get("max_attendees")
 
-        num_attendees = attendees_qs.all().count()
+        if max_attendees < num_attendees:
+            validation_error["max_attendees"] = (
+                "max_attendees must be higher or equal to the number of attendees selected."
+            )
+
+        volunteers_emails = attrs.get("volunteers")
+        num_volunteers = len(volunteers_emails)
+        max_volunteers = attrs.get("max_volunteers")
+
+        if max_volunteers < num_volunteers:
+            validation_error["max_volunteers"] = (
+                "max_volunteers must be higher or equal to the number of volunteers selected."
+            )
+
         max_capacity = attrs.get("capacity")
-        if max_capacity < num_attendees:
-            validation_error[
-                "capacity"
-            ] = "The max capacity must be higher or equal to the number of attendees selected."
+        if max_capacity < max_attendees + max_volunteers:
+            validation_error["capacity"] = (
+                "max_capacity must be higher or equal to max_attendees + max_volunteers."
+            )
+
+        if max_capacity < num_attendees + num_volunteers:
+            validation_error["capacity"] = (
+                "max_capacity must be higher or equal to the total number of people attending."
+            )
 
         start_date = attrs.get("start_date")
         if start_date <= datetime.now(timezone.utc):
@@ -55,14 +130,6 @@ class EventSerializer(ModelSerializer):
             validation_error["end_date"] = "The end date must be in the future."
         if end_date <= start_date:
             validation_error["end_date"] = "The end date must be after the start date."
-
-        num_volunteers = attendees_qs.filter(volunteer__isnull=False).count()
-        max_volunteers = attrs.get("max_volunteers")
-
-        if max_volunteers < num_volunteers:
-            validation_error[
-                "max_volunteers"
-            ] = "The max number of volunteers must be higher or equal to the number of volunteers selected."
 
         if validation_error:
             raise serializers.ValidationError(validation_error)
--- nexong/api/Event/views.py
+++ nexong/api/Event/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from ...models import *
-from .eventSerializer import EventSerializer
+from .eventSerializer import EventSerializer, LessonEventSerializer
 from .. import permissions
 
 
@@ -10,6 +10,18 @@ class EventApiViewSet(ModelViewSet):
     queryset = Event.objects.all()
     http_method_names = ["get", "post", "put", "delete"]
     serializer_class = EventSerializer
+    permission_classes = [permissions.isAdminOrReadOnly]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class LessonEventApiViewSet(ModelViewSet):
+    queryset = Event.objects.all()
+    http_method_names = ["get", "post", "put", "delete"]
+    serializer_class = LessonEventSerializer
     permission_classes = [permissions.isAdminOrReadOnly]
 
     def destroy(self, request, *args, **kwargs):
