ae23d1855352f456ed20d3deccbfd382657d5278
pedlopruz <pedlopruz@users.noreply.github.com>
2024-03-02 20:33:58 +0000

    refactor: Format code with Black

--- nexong/api/routers.py
+++ nexong/api/routers.py
@@ -9,5 +9,11 @@ router_api.register(prefix="user", viewset=UserApiViewSet, basename="user")
 router_api.register(prefix="meeting", viewset=MeetingApiViewSet, basename="meeting")
 router_api.register(prefix="event", viewset=EventApiViewSet, basename="event")
 router_api.register(prefix="lesson", viewset=LessonApiViewSet, basename="lesson")
-router_api.register(prefix="lessonAttendance",viewset=LessonAttendanceApiViewSet,basename="lessonAttendance")
-router_api.register(prefix="lesson-event", viewset=LessonEventApiViewSet, basename="lessonevent")
+router_api.register(
+    prefix="lessonAttendance",
+    viewset=LessonAttendanceApiViewSet,
+    basename="lessonAttendance",
+)
+router_api.register(
+    prefix="lesson-event", viewset=LessonEventApiViewSet, basename="lessonevent"
+)
