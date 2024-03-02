f5513067498323a5c2e19b62761df936d535ab14
Ahydul <ahydul1@gmail.com>
2024-03-02 16:41:38 +0100

    feat: Add Donation serializer

--- nexong/api/routers.py
+++ nexong/api/routers.py
@@ -2,8 +2,10 @@ from rest_framework.routers import DefaultRouter
 from .Authentication.views import *
 from .Meeting.views import *
 from .Event.views import *
+from .Donation.views import *
 
 router_api = DefaultRouter()
 router_api.register(prefix="user", viewset=UserApiViewSet, basename="user")
 router_api.register(prefix="meeting", viewset=MeetingApiViewSet, basename="meeting")
 router_api.register(prefix="event", viewset=EventApiViewSet, basename="event")
+router_api.register(prefix="donation", viewset=DonationApiViewSet, basename="donation")
