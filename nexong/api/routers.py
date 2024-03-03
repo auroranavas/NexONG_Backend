fcdb4a13f810a1e8393af233bfae37a4fad1e1fd
JuanluRM <juanluis.ruano.muriedas@gmail.com>
2024-03-03 14:10:25 +0100

    fix: deleted wrong routing from volunteer

--- nexong/api/routers.py
+++ nexong/api/routers.py
@@ -8,17 +8,7 @@ router_api = DefaultRouter()
 router_api.register(prefix="user", viewset=UserApiViewSet, basename="user")
 router_api.register(prefix="meeting", viewset=MeetingApiViewSet, basename="meeting")
 router_api.register(prefix="event", viewset=EventApiViewSet, basename="event")
-router_api.register(
-    prefix="lesson-event", viewset=LessonEventApiViewSet, basename="lessonevent"
-)
-router_api.register(
-    prefix="student-evaluation",
-    viewset=StudentEvaluationApiViewSet,
-    basename="studentevaluation",
-)
-router_api.register(
-    prefix="evaluation-type",
-    viewset=EvaluationTypeApiViewSet,
-    basename="evaluationtype",
-)
-router_api.register(prefix="volunteer", viewset=EventApiViewSet, basename="volunteer")
+router_api.register(prefix="lesson-event", viewset=LessonEventApiViewSet, basename="lessonevent")
+router_api.register(prefix="student-evaluation",viewset=StudentEvaluationApiViewSet,basename="studentevaluation")
+router_api.register(prefix="evaluation-type",viewset=EvaluationTypeApiViewSet,basename="evaluationtype")
+
