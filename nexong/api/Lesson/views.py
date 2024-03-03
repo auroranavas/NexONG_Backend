1d152d55069aab97bf9e9e3dd058900e270ce6d8
pedlopruz <plopezr2011@gmail.com>
2024-03-03 12:32:56 +0100

    fix: Codacy Errors Duplication

--- nexong/api/Lesson/views.py
+++ nexong/api/Lesson/views.py
@@ -31,12 +31,6 @@ class LessonAttendanceApiViewSet(ModelViewSet):
     serializer_class = LessonAttendanceSerializer
     permission_classes = [permissions.isAdminOrReadOnly]
 
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
