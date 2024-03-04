0718dfb5e3a442a823489dcd8f4227066f43c72f
auroranavas <navasaurora@outlook.com>
2024-03-04 17:11:18 +0100

    a ve si cuela

--- local_settings_deploy.py
+++ local_settings_deploy.py
@@ -27,7 +27,7 @@ MIDDLEWARE = [
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = os.getenv("SECRET_THREE")
+STATICFILES_STORAGE = os.environ.get("SECRET_THREE")
 
 """
 BASEURL = "https://{}".format(os.environ.get("RENDER_EXTERNAL_HOSTNAME"))
